89a3be2d04021e91237449bc571e0ffb87de5571
xiomy-20 <106261141+xiomy-20@users.noreply.github.com>
2023-01-10 15:40:45 -0500

    Update __init__.py

--- product_price2/models/__init__.py
+++ product_price2/models/__init__.py
@@ -1,3 +1,25 @@
-# -*- coding: utf-8 -*-
+<?xml version="1.0"?>
+<t name="Web layout" t-name="web.layout">&lt;!DOCTYPE html&gt;
+        <html t-att="html_data or {}">
+            <head>
+                <meta charset="utf-8"/>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
 
-from . import product_template
+                <title t-esc="title or 'Odoo'"/>
+                <link type="image/x-icon" rel="shortcut icon" t-att-href="x_icon or '/web/static/src/img/favicon.ico'"/>
+
+                <script id="web.layout.odooscript" type="text/javascript">
+                    var odoo = {
+                        csrf_token: "<t t-esc="request.csrf_token(None)"/>",
+                        debug: "<t t-esc="debug"/>",
+                    };
+                </script>
+
+                <t t-raw="head or ''"/>
+            </head>
+            <body t-att-class="body_classname">
+                <t t-raw="0"/>
+            </body>
+        </html>
+    </t>
